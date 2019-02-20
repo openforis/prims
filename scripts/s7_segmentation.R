e58dc0723116994afd1d5601188127e873aad4f4
lecrabe <remi.dannunzio@gmail.com>
2019-02-20 08:44:59 +0700

    segmentation

---
+++ scripts/s7_segmentation.R
@@ -0,0 +1,63 @@
+####################################################################################################
+## Segmentation of the mosaic
+## remi.dannunzio@fao.org
+## 2018/02/05
+####################################################################################################
+####################################################################################################
+options(stringsAsFactors=FALSE)
+workdir <- moz_dir
+
+#################### PERFORM SEGMENTATION USING THE OTB-SEG ALGORITHM
+params <- c(3,   # radius of smoothing (pixels)
+            16,  # radius of proximity (pixels)
+            0.1, # radiance threshold 
+            50,  # iterations of algorithm
+            5)  # segment minimum size (pixels)
+
+tiles <- list.files(moz_dir,pattern=glob2rx("*.tif"))
+i<-1
+for(i in 1:length(tiles)){
+  input <- tiles[i]
+  system(sprintf("otbcli_MeanShiftSmoothing -in %s -fout %s -foutpos %s -spatialr %s -ranger %s -thres %s -maxiter %s",
+                 paste0(workdir,input),
+                 paste0(workdir,"tmp_smooth_",paste0(params,collapse = "_"),".tif"),
+                 paste0(workdir,"tmp_position_",paste0(params,collapse = "_"),".tif"),
+                 params[1],
+                 params[2],
+                 params[3],
+                 params[4]
+  ))
+
+  system(sprintf("otbcli_LSMSSegmentation -in %s -inpos %s -out %s -spatialr %s -ranger %s -minsize 0 -tmpdir %s -tilesizex 512 -tilesizey 512",
+                 paste0(workdir,"tmp_smooth_",paste0(params,collapse = "_"),".tif"),
+                 paste0(workdir,"tmp_position_",paste0(params,collapse = "_"),".tif"),
+                 paste0(workdir,"tmp_seg_lsms_",paste0(params,collapse = "_"),".tif"),
+                 params[1],
+                 params[2],
+                 workdir
+  ))
+
+  system(sprintf("otbcli_LSMSSmallRegionsMerging -in %s -inseg %s -out %s -minsize %s -tilesizex 512 -tilesizey 512",
+                 paste0(workdir,"tmp_smooth_",paste0(params,collapse = "_"),".tif"),
+                 paste0(workdir,"tmp_seg_lsms_",paste0(params,collapse = "_"),".tif"),
+                 paste0(seg_dir,"seg_",input),
+                 params[5]
+  ))
+
+  system(sprintf("rm -r %s",
+                paste0(workdir,"tmp*.tif")))
+  
+  #################### CREATE data mask
+  system(sprintf("gdal_calc.py -A %s --co COMPRESS=LZW --overwrite --outfile=%s --calc=\"%s\"",
+                 paste0(workdir,input),
+                 paste0(seg_dir,"mask_",input),
+                 "(A>0)"
+  ))
+
+}
+
+# ######################### POLYGONIZE
+# system(sprintf("gdal_polygonize.py -f \"ESRI Shapefile\" %s %s",
+#                paste0(seg_dir,"seg_lsms_tile_1_param_",paste0(params,collapse = "_"),".tif"),
+#                paste0(seg_dir,"seg_lsms_tile_1_",paste0(params,collapse = "_"),".shp")
+# ))
